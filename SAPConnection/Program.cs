11053d11d68d6fefc867d5829af5594f13a0abbb
qyweiyy <qyweiyy@163.com>
2021-11-08 17:18:28 +0800

    create project
    此项目是通过 使用 SapNrRfc 、SapDotNetConnector3 分别基于 .net5 .net framework 4.0 开发的一套用于连接（调取） SAP function 的实例。
    希望能对大家有所帮助。

---
+++ SAPConnection/Program.cs
@@ -0,0 +1,220 @@
+﻿using Microsoft.Extensions.Logging;
+using SapNwRfc;
+using System;
+using System.Data;
+
+namespace SAPConnection
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("开始连接SAP!");
+            //确保存在 SAP RFC SDK 二进制文件
+            SapLibrary.EnsureLibraryPresent();
+            //与 SAP 网络编织器连接
+            string connectionString = "AppServerHost=172.16.101.207; SystemNumber=00; User=WMS03; Password=Aiko12345; Client=800; Language=ZH; PoolSize=10; Trace=20";
+            using var connection = new SapConnection(connectionString);
+            connection.Connect();
+
+            // RFC 服务器通用处理程序
+            sapConnection(connectionString);
+
+            //没有输入或输出参数的呼叫功能
+            using var someFunction = connection.CreateFunction("ZOAFM_HWYD_INFO");
+            //someFunction.Invoke();
+
+            //具有输入参数但没有输出参数的呼叫功能
+            var result = someFunction.Invoke<SomeFunctionResult>(new SomeFunctionParameters
+            {
+                Budat = "2021-07-18",
+                //Werks = "2000",
+                //Aufnr = "",
+                Matnr = "60001297",
+                //Lgort = ""
+            });
+        }
+        /// <summary>
+        /// RFC 服务器通用处理程序
+        /// </summary>
+        public static void sapConnection(string connectionString)
+        {
+            //SapServer.InstallGenericServerFunctionHandler(connectionString, function =>
+            //{
+            //    switch (function.Name)
+            //    {
+            //        case "ZFM_WMS2_006":
+            //            var parameters = function.GetParameters<SomeFunctionParameters>();
+            //            function.SetResult(new SomeFunctionResult { Abc = "Some Value" });
+            //            break;
+            //    }
+            //});
+        }
+        /// <summary>
+        /// RFC 服务器
+        /// </summary>
+        public void Server(string connectionString)
+        {
+            using var server = SapServer.Create(connectionString);
+            server.Launch();
+        }
+    }
+    /// <summary>
+    /// 通过创建从SapConnection 参数继承的类，可以添加其他连接参数：
+    /// </summary>
+    public class MySapConnectionParameters : SapConnectionParameters
+    {
+        [SapName("CST_PARAM")]
+        public string CustomParameter { get; set; }
+    }
+    #region 具有输入和输出参数的呼叫功能
+    class SomeFunctionParameters
+    {
+        /// <summary>
+        /// 过账日期
+        /// </summary>
+        [SapName("IM_BUDAT")]
+        public string Budat { get; set; }
+        /// <summary>
+        /// 工厂
+        /// </summary>
+        [SapName("IM_WERKS")]
+        public string Werks { get; set; }
+        /// <summary>
+        /// 订单号
+        /// </summary>
+        [SapName("IM_AUFNR")]
+        public string Aufnr { get; set; }
+        /// <summary>
+        /// MATNR
+        /// </summary>
+        [SapName("IM_MATNR")]
+        public string Matnr { get; set; }
+        /// <summary>
+        /// 库存地点
+        /// </summary>
+        [SapName("IM_LGORT")]
+        public string Lgort { get; set; }
+        /// <summary>
+        /// 移动类型(库存管理)
+        /// </summary>
+        [SapName("IM_BWART")]
+        public string Bwart { get; set; }
+    }
+
+    //class SomeFunctionResult
+    //{
+    //    /// <summary>
+    //    /// 单字符标记
+    //    /// </summary>
+    //    [SapName("EX_CODE")]
+    //    public string Code { get; set; }
+    //    /// <summary>
+    //    /// 文本字段长度 200
+    //    /// </summary>
+    //    [SapName("EX_MESSAGE")]
+    //    public string Message { get; set; }
+    //}
+    #endregion
+    #region 定义嵌套结构的模型
+
+    //class SomeFunctionResult
+    //{
+    //    /// <summary>
+    //    /// 单字符标记
+    //    /// </summary>
+    //    [SapName("EX_CODE")]
+    //    public string Code { get; set; }
+    //    /// <summary>
+    //    /// 文本字段长度 200
+    //    /// </summary>
+    //    [SapName("EX_MESSAGE")]
+    //    public string Message { get; set; }
+    //    [SapName("OT_DATA")]
+    //    public DataTable table { get; set; }
+    //}
+
+    //class SomeFunctionResultAddress
+    //{
+    //    /// <summary>
+    //    /// 单字符标记
+    //    /// </summary>
+    //    [SapName("EX_CODE")]
+    //    public string Code { get; set; }
+    //    /// <summary>
+    //    /// 文本字段长度 200
+    //    /// </summary>
+    //    [SapName("EX_MESSAGE")]
+    //    public string Message { get; set; }
+    //}
+    #endregion
+
+
+    #region 使用嵌套表定义模型
+    class SomeFunctionResult
+    {
+        /// <summary>
+        /// 单字符标记
+        /// </summary>
+        [SapName("EX_CODE")]
+        public string Code { get; set; }
+        /// <summary>
+        /// 文本字段长度 200
+        /// </summary>
+        [SapName("EX_MESSAGE")]
+        public string Message { get; set; }
+
+        [SapName("OT_DATA")]
+        public SomeFunctionResultItem[] Items { get; set; }
+    }
+
+    class SomeFunctionResultItem
+    {
+        public string MBLNR { get; set; }
+        public string MJAHR { get; set; }
+        public string ZEILE { get; set; }
+        public string BUDAT { get; set; }
+        public string WERKS { get; set; }
+        public string BWART { get; set; }
+        public string MATNR { get; set; }
+        public string LGORT { get; set; }
+        public string UMLGO { get; set; }
+        public string CHARG { get; set; }
+        public string LIFNR { get; set; }
+        public string MENGE { get; set; }
+        public string MEINS { get; set; }
+        public string EBELN { get; set; }
+        public string EBELP { get; set; }
+        public string DMBTR { get; set; }
+        public string ZJLFL { get; set; }
+        public string NAME1 { get; set; }
+        public string SORTL { get; set; }
+        public string MATKL { get; set; }
+        public string MAKTX { get; set; }
+        public string AUFNR { get; set; }
+        public string CPUDT { get; set; }
+        public string CPUTM { get; set; }
+        public string FEVOR { get; set; }
+    }
+    #endregion
+
+    #region 将属性排除在映射中
+    //class SomeFunctionParameters
+    //{
+    //    [SapIgnore]
+    //    public string IgnoredProperty { get; set; }
+
+    //    [SapName("SOME_FIELD")]
+    //    public string SomeField { get; set; }
+    //}
+
+    //class SomeFunctionResult
+    //{
+    //    [SapIgnore]
+    //    public string IgnoredProperty { get; set; }
+
+    //    [SapName("SOME_FIELD")]
+    //    public string SomeField { get; set; }
+    //}
+    #endregion
+}
---
+++ SAPDotNetConnector/Program.cs
@@ -0,0 +1,292 @@
+﻿using SAP.Middleware.Connector;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Configuration;
+
+namespace SAPDotNetConnector
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, string> keyValues = new Dictionary<string, string>();
+            keyValues.Add("IM_BUDAT","2021-07-18");
+            List<string> columns = new List<string>();
+            SAPServerHelper sapHelper = new SAPServerHelper();
+            var result =sapHelper.getSapFunctionToTable("ZOAFM_HWYD_INFO", keyValues, "OT_DATA");
+            Console.WriteLine(result.Rows.Count);
+            foreach(DataColumn item in result.Columns)
+            {
+                Console.WriteLine(item.ColumnName+"\t");
+            }
+            Console.WriteLine("Press any key outer.");
+            Console.ReadKey();
+        }
+    }
+
+    public abstract class Common
+    {
+        /// <summary>
+        /// 获取SAP配置信息
+        /// </summary>
+        public static SAPServerInfoModel GetSapServerInfo()
+        {
+            SAPServerInfoModel sapInfo = new SAPServerInfoModel
+            {
+                NAME = ConfigurationManager.AppSettings["NAME"],
+                ASHOST = ConfigurationManager.AppSettings["ASHOST"],
+                CLIENT = ConfigurationManager.AppSettings["CLIENT"],
+                USER = ConfigurationManager.AppSettings["USER"],
+                PASSWD = ConfigurationManager.AppSettings["PASSWD"],
+                SYSNR = ConfigurationManager.AppSettings["SYSNR"],
+                LANG = ConfigurationManager.AppSettings["LANG"]
+            };
+            return sapInfo;
+        }
+    }
+
+    public class SAPConfiguration : IDestinationConfiguration
+    {
+        public bool ChangeEventsSupported()
+        {
+            return false;
+        }
+
+        public event RfcDestinationManager.ConfigurationChangeHandler ConfigurationChanged;
+
+        private SAPServerInfoModel info;
+
+        public SAPConfiguration(SAPServerInfoModel info)
+        {
+            this.info = info;
+        }
+
+        public RfcConfigParameters GetParameters(string destinationName)
+        {
+            try
+            {
+                if (destinationName.Equals("PRD"))
+                {
+                    RfcConfigParameters parms = new RfcConfigParameters();
+                    parms.Add(RfcConfigParameters.Name, info.NAME);
+                    parms.Add(RfcConfigParameters.AppServerHost, info.ASHOST);
+                    parms.Add(RfcConfigParameters.Client, info.CLIENT);
+                    parms.Add(RfcConfigParameters.User, info.USER);
+                    parms.Add(RfcConfigParameters.Password, info.PASSWD);
+                    parms.Add(RfcConfigParameters.Language, info.LANG);
+                    parms.Add(RfcConfigParameters.SystemNumber, info.SYSNR);
+                    return parms;
+                }
+                else
+                {
+                    return null;
+                }
+            }
+            catch (Exception ex)
+            {
+                return null;
+            }
+        }
+    }
+
+    public class SAPServerHelper : IDisposable
+    {
+        private RfcDestination prd;//SAP Rfc定义声明对象
+
+        private SAPConfiguration sapConfig;//SAP配置对象
+
+        #region 创建SAP连接对象
+        /// <summary>
+        /// 创建SAP连接对象
+        /// </summary>
+        public SAPServerHelper()
+        {
+            SAPServerInfoModel sapInfo = Common.GetSapServerInfo();
+            sapConfig = new SAPConfiguration(sapInfo);
+            prd = RfcDestinationManager.GetDestination(sapConfig.GetParameters("PRD"));
+        }
+        #endregion
+
+        #region 与SAP进行数据交互（MES->SAP && SAP->MES） 
+        /// <summary>
+        /// 与SAP进行数据交互（MES->SAP && SAP->MES）
+        /// </summary>
+        /// <param name="funName">SAP接口名</param>
+        /// <param name="lstParameters">SAP传入参数列表</param>
+        /// <param name="TableName">SAP表名</param>
+        /// <param name="rtbInputList">SAP传入参数Table列表</param>
+        /// <param name="outParameters">返回值列表</param>
+        /// <param name="outList">返回参数列表</param>
+        /// <returns></returns>
+        public DataTable getSapFunctionToTable(string funName, Dictionary<string, string> lstParameters, string TableName,
+            Dictionary<IRfcTable, string> rtbInputList, out Dictionary<string, string> outParameters, List<string> outList)
+        {
+            Dictionary<string, string> outDataList = new Dictionary<string, string>();
+
+            DataTable dt = new DataTable();
+            RfcRepository repo = prd.Repository;
+            //输入
+            IRfcFunction function = repo.CreateFunction(funName);
+            foreach (var item in lstParameters)
+            {
+                function.SetValue(item.Key, item.Value);
+            }
+            foreach (var item in rtbInputList)
+            {
+                function.SetValue(item.Value, item.Key);
+            }
+            function.Invoke(prd);
+
+            //输出
+            IRfcTable rtb = function.GetTable(TableName);
+            foreach (var item in outList)
+            {
+                outDataList[item] = function.GetString(item);
+            }
+            outParameters = outDataList;
+            dt = CreateTable(rtb, TableName);
+            return dt;
+        }
+        #endregion
+
+        #region 与SAP进行数据交互（MES->SAP）返回Table
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="funName">RFC名称</param>
+        /// <param name="lstParameters">参数列表</param>
+        /// <param name="TableName">返回值Table结构名称</param>
+        /// <returns></returns>
+        public DataTable getSapFunctionToTable(string funName, Dictionary<string, string> lstParameters,
+            string TableName)
+        {
+            DataTable dt = new DataTable();
+            RfcRepository repo = prd.Repository;
+            IRfcFunction function = repo.CreateFunction(funName);
+            foreach (var item in lstParameters)
+            {
+                function.SetValue(item.Key, item.Value);
+            }
+            function.SetParameterActive(0, true);
+            function.Invoke(prd);
+            IRfcTable rtb = function.GetTable(TableName);
+            dt = CreateTable(rtb, TableName);
+            return dt;
+        }
+        #endregion
+
+        #region 与SAP进行数据交互（MES->SAP）返回多个字符串+Table
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="funName">RFC名称</param>
+        /// <param name="lstParameters">参数列表</param>
+        /// <param name="TableName">返回值Table结构名称</param>
+        /// <param name="outParameters">接收返回字符串List</param>
+        /// <param name="outList">返回参数列表</param>
+        /// <returns></returns>
+        public DataTable getSapFunctionToTable(string funName, Dictionary<string, string> lstParameters,
+            string TableName, out Dictionary<string, string> outParameters, List<string> outList)
+        {
+            Dictionary<string, string> outDataList = new Dictionary<string, string>();
+            DataTable dt = new DataTable();
+            RfcRepository repo = prd.Repository;
+            IRfcFunction function = repo.CreateFunction(funName);
+            foreach (var item in lstParameters)
+            {
+                function.SetValue(item.Key, item.Value);
+            }
+            function.SetParameterActive(0, true);
+            function.Invoke(prd);
+            IRfcTable rtb = function.GetTable(TableName);
+            foreach (var item in outList)
+            {
+                outDataList[item] = function.GetString(item);
+            }
+            outParameters = outDataList;
+            dt = CreateTable(rtb, TableName);
+            return dt;
+        }
+        #endregion
+
+        #region 根据返回的数据,生成DataTable
+        /// <summary>
+        /// 根据返回的数据,生成DataTable
+        /// </summary>
+        /// <param name="rtb"></param>
+        /// <param name="TableName"></param>
+        /// <returns></returns>
+        private DataTable CreateTable(IRfcTable rtb, string TableName)
+        {
+            DataTable dt = new DataTable(TableName);
+
+            //建立表结构，设定表头
+            for (int i = 0; i <= rtb.ElementCount - 1; i++)
+            {
+                DataColumn col = new DataColumn(rtb.GetElementMetadata(i).Name);
+                dt.Columns.Add(col);
+            }
+            //填充表数据  
+            for (int k = 0; k <= rtb.RowCount - 1; k++)
+            {
+                DataRow dr = dt.NewRow();
+                for (int i = 0; i <= rtb.ElementCount - 1; i++)
+                {
+                    dr[i] = rtb[k][i].GetValue();
+                }
+                dt.Rows.Add(dr);
+            }
+            return dt;
+        }
+        #endregion
+
+        #region 定义要给SAP传送的数据列表(把实际的Table数据转换成SAP可识别的Table数据)
+        /// <summary>
+        /// 定义要给SAP传送的数据列表(把实际的Table数据转换成SAP可识别的Table数据)
+        /// </summary>
+        /// <param name="dt"></param>
+        /// <param name="funName"></param>
+        /// <param name="TableName"></param>
+        /// <returns></returns>
+        public IRfcTable CreateRfcTable(DataTable dt, string funName, string TableName)
+        {
+            RfcRepository repo = prd.Repository;
+            IRfcFunction Z_RFC_ZCOX = repo.CreateFunction(funName);
+            IRfcTable rtb = Z_RFC_ZCOX.GetTable(TableName, true);
+            for (int i = 0; i < dt.Rows.Count; i++)
+            {
+                rtb.Append();
+                foreach (DataColumn col in dt.Columns)
+                {
+                    rtb.SetValue(rtb[i][col.ColumnName].Metadata.Name, dt.Rows[i][col.ColumnName].ToString());
+                }
+            }
+            return rtb;
+        }
+        #endregion
+
+        #region 释放资源
+        /// <summary>
+        /// 释放资源
+        /// </summary>
+        public void Dispose()
+        {
+            RfcDestinationManager.UnregisterDestinationConfiguration(sapConfig);
+        }
+        #endregion
+    }
+
+    public class SAPServerInfoModel
+    {
+        public string NAME { get; set; }
+        public string ASHOST { get; set; }
+        public string CLIENT { get; set; }
+        public string USER { get; set; }
+        public string PASSWD { get; set; }
+        public string SYSNR { get; set; }
+        public string LANG { get; set; }
+    }
+}
